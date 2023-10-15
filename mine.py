3a91bafbd6e607d10d55d37a0ae17afd6506d1a8
Adekanmbi Samuel <samueladekanmbi2@gmail.com>
2023-10-15 22:50:47 +0100

    i love DevOps

--- mine.py
+++ mine.py
@@ -1,6 +1,6 @@
 # Define variables
-name = "PUT YOUR NAME"
-email = 'PUT YOUR EMAIL'
+name = "ADEKANMBI SAMUEL ADETAYO"
+email = 'samueladekanmbi2@gmail.com'
 
 # Print the name and email address
 print("Name:", name)
